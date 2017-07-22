413802bdfcc97d6ee9749ed4ff5bc478e881b2b5
Kumar G <gch.kumar@gmail.com>
2017-07-22 05:46:17 +0530

    Unit test files added

--- src/com/vaannila/service/UserServiceImpl.java
+++ src/com/vaannila/service/UserServiceImpl.java
@@ -8,7 +8,7 @@ public class UserServiceImpl implements UserService {
 	public void add(User user) {
 		//Persist the user object here. 
 		System.out.println("User added successfully");
-
+ 
 	}
 
 }
